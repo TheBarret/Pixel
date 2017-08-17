aa0793fec704d01069e798933a7761c7bddcb99f
Barret <format.e.drive@gmail.com>
2017-08-17 19:59:26 +0200

    Add files via upload

---
+++ Pixel Editor/bin/Debug/Counter[128x64].asm
@@ -0,0 +1,40 @@
+﻿;************************************
+; Program: Display counter
+;************************************
+clear | jmp [main]
+
+:counter	0
+:string		"Count:"
+:x			10
+:y			10
+:i			0
+:timer		0
+
+:main	call [print_text]			;print out text
+	storev [x] #45			;increase x
+:loop	if [counter] #100 call [reset]	;reset if 100
+	call [print_counter]
+	jmp [loop]
+
+:print_counter
+	printv [x][y][counter]		;Draw counter
+	call [wait]				;Hang back..
+	printv [x][y][counter]		;Reverse last draw
+	inc [counter] #1			;increase counter
+	return
+
+:print_text
+	print [x][y][i][string]
+	inc [i] #1
+	inc [x] #6
+	if [i] #6 return
+	jmp [print_text]
+
+:reset	storev [counter] #0
+	return
+
+:wait	storev [timer] #60
+:wait_loop
+	dec [timer] #1
+	if [timer] #0 return
+	jmp [wait_loop]
---
+++ Pixel Editor/bin/Debug/Maze2[128x64].asm
@@ -0,0 +1,137 @@
+﻿;************************************
+; Program: Continuous Maze Generator
+; Version: 2
+;************************************
+clear | seed 0xff | jmp [init]
+
+:x		0
+:y		0
+:index		0
+:di		0
+:s		6
+:t		0
+:ti		0
+:tx		25
+:ty		7
+:author	"Made by Barret"
+
+:init	call [intro]
+:main	random #11
+	store [index]
+	if [index] #0 call [empty]
+	if [index] #1 call [f1]
+	if [index] #2 call [f2]
+	if [index] #3 call [f3]
+	if [index] #4 call [f4]
+	if [index] #5 call [f5]
+	if [index] #6 call [f6]
+	if [index] #7 call [f7]
+	if [index] #8 call [f8]
+	if [index] #9 call [f9]
+	if [index] #10 call [f10]
+	if [index] #11 call [f11]
+	inc [x] #8
+	ifl [x] #128 jmp [main]
+	storev [x] #0
+	scroll [di][s]
+	call [sleep]
+	jmp [main]
+
+:intro
+	if [ti] #14 return
+	print [tx][ty][ti][author]
+	inc [ti] #1
+	inc [tx] #5
+	jmp [intro]
+	
+
+:f1	draw [x][y][a] | return
+:f2	draw [x][y][b] | return
+:f3	draw [x][y][c] | return
+:f4	draw [x][y][d] | return
+:f5	draw [x][y][e] | return
+:f6	draw [x][y][f] | return
+:f7	draw [x][y][g] | return
+:f8	draw [x][y][h] | return
+:f9	draw [x][y][i] | return
+:f10	draw [x][y][j] | return
+:f11	draw [x][y][k] | return
+:empty	draw [x][y][n] | return
+
+:sleep storev [t] #50
+:sloop if [t] #0 return
+	dec [t] #1
+	jmp [sloop]	
+
+:a	.00000000
+	.00000000
+	.01010101
+	.10101010
+	.00000000
+	.00000000
+:b	.00010000
+	.00001000
+	.00010000
+	.00001000
+	.00010000
+	.00001000
+:c	.00011000
+	.00011000
+	.11111111
+	.11111111
+	.00011000
+	.00011000
+:d	.00000000
+	.00000000
+	.11111111
+	.11111111
+	.00011000
+	.00011000
+:e	.00011000
+	.00011000
+	.11111111
+	.11111111
+	.00000000
+	.00000000
+:f	.00000000
+	.00000000
+	.00011111
+	.00011111
+	.00011000
+	.00011000
+:g	.00000000
+	.00000000
+	.11111000
+	.11111000
+	.00011000
+	.00011000
+:h	.00011000
+	.00011000
+	.00011111
+	.00011111
+	.00000000
+	.00000000
+:i	.00011000
+	.00011000
+	.11111000
+	.11111000
+	.00000000
+	.00000000
+:j	.00011000
+	.00011000
+	.11111000
+	.11111000
+	.00011000
+	.00011000
+:k	.00011000
+	.00011000
+	.00011111
+	.00011111
+	.00011000
+	.00011000
+:n	.00000000
+	.00000000
+	.00011000
+	.00011000
+	.00000000
+	.00000000
